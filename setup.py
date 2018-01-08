1762c8cf76df3fab75542585f4d74a754273ebf4
Taylor Nation <tnation14@gmail.com>
2018-01-08 16:56:38 -0500

    Check-manifest fixes

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@
 
 from setuptools import setup, find_packages
 from glob import glob
-import io
+
 from os.path import (
-    join,
-    dirname,
     basename,
     splitext
 )
@@ -19,33 +17,15 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-
-def read(*names, **kwargs):
-    return io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
-    ).read()
-
-requirements = [
-    read('requirements.txt')
-]
-
-setup_requirements = [
-    # TODO(notthatbreezy): put setup requirements (distutils extensions, etc.) here
-]
-
-test_requirements = [
-    # TODO: put package test requirements here
-]
-
 setup(
     name='pystac',
-    version='0.1.0',
-    description="Python command line utilities for interacting with and creating STAC compliant files.",
+    version='0.1.1',
+    description=("Python command line utilities for interacting with and"
+                 "creating STAC compliant files."),
     long_description=readme + '\n\n' + history,
     author="Raster Foundry",
     author_email='info@rasterfoundry.com',
-    url='https://github.com/notthatbreezy/pystac',
+    url='https://github.com/rasterfoundry/pystac',
     packages=find_packages(),
     py_modules=[splitext(basename(path))[0] for path in glob('pystac/*.py')],
     entry_points={
@@ -54,24 +34,32 @@ setup(
         ]
     },
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=[
+        'python-dateutil==2.6.1',
+        'rasterio==1.0a12',
+        'Click>=6.0',
+        'geojson==2.3.0',
+        'requests==2.18.0',
+        'typing==3.6.2;python_version<"3.5"'
+    ],
     license="Apache Software License 2.0",
     zip_safe=False,
-    keywords='pystac',
+    keywords=[
+        'pystac',
+        'imagery',
+        'raster',
+        'catalog',
+        'STAC'
+    ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
-    test_suite='tests',
-    tests_require=test_requirements,
-    setup_requires=setup_requirements,
+    test_suite='tests'
 )
