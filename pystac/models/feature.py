4457c1657ff0e62ea04bc85f27697491474e6f8a
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-04-22 11:59:12 +0200

    Add marshalling of feature model

--- pystac/models/feature.py
+++ pystac/models/feature.py
@@ -1,18 +1,20 @@
-from typing import List
-
 import geojson
 from geojson import Polygon
 
-from pystac.models.asset import Asset
+from pystac.models.asset import AssetSchema
 from pystac.models.base import STACObject
-from pystac.models.link import Link
-from pystac.models.properties import Properties
+from pystac.models.link import LinkSchema
+from pystac.models.properties import PropertiesSchema
+from marshmallow import (
+    Schema,
+    fields
+)
 
 
 class Feature(STACObject):
-    def __init__(self, item_id: str,
-                 geometry: Polygon, properties: Properties,
-                 links: List[Link], assets: List[Asset]):
+    def __init__(self, item_id,
+                 geometry, properties,
+                 links, assets):
         """STAC Catalog item
 
         Args:
@@ -34,7 +36,7 @@ class Feature(STACObject):
         return [min(lats), min(lngs), max(lats), max(lngs)]
 
     @property
-    def dict(self) -> dict:
+    def dict(self):
         return dict(
             type='Feature',
             id=self.id,
@@ -44,3 +46,18 @@ class Feature(STACObject):
             links=[link.dict for link in self.links],
             assets=[asset.dict for asset in self.assets]
         )
+    
+    @property
+    def json(self):
+        return FeatureSchema().dumps(
+            self
+        )
+
+
+class FeatureSchema(Schema):
+
+    links = fields.Nested(LinkSchema, many=True)
+    properties = fields.Nested(PropertiesSchema)
+    geometry = fields.Dict()
+    id = fields.Str()
+    assets = fields.Nested(AssetSchema, many=True)
