c4fede53b61da14bd7171c0b280f2b7db44cc342
James Santucci <james.santucci@gmail.com>
2019-11-19 11:45:07 -0500

    Add test for asset absolute href construction

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,7 +1,7 @@
 import unittest
 import json
 
-from pystac import Item
+from pystac import Asset, Item
 from tests.utils import (TestCases, test_to_from_dict)
 
 
@@ -24,3 +24,15 @@ class ItemTest(unittest.TestCase):
         self.assertEqual(item.assets['analytic'].properties['product'],
                          'http://cool-sat.com/catalog/products/analytic.json')
         self.assertIsNone(item.assets['thumbnail'].properties)
+
+    def test_asset_absolute_href(self):
+        m = TestCases.get_path(
+            'data-files/itemcollections/sample-item-collection.json')
+        with open(m) as f:
+            item_dict = json.load(f)['features'][0]
+        item = Item.from_dict(item_dict)
+        rel_asset = Asset('./data.geojson')
+        rel_asset.set_owner(item)
+        expected_href = 'http://stacspec.org/data.geojson'
+        actual_href = rel_asset.get_absolute_href()
+        self.assertEqual(expected_href, actual_href)
