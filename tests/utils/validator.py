f7ed1fce5c483b8f0020b5537784fe84fbafceec
lossyrob <rdemanuele@gmail.com>
2020-02-27 12:15:15 -0500

    Modify SchemaValidator to cache schemas to speed up validation.
    In STAC 0.9.0 there is a heavier use of remote schemas; this slowed
    down the jsonschema validation over larger catalogs. These changes
    help speed things up by caching all requiured remote schemas and
    passing them to the RefResolver, so that the remote schemas listed in
    "schemas" and "aux_schemas" will only be read once.

--- tests/utils/validator.py
+++ tests/utils/validator.py
@@ -22,34 +22,64 @@ class SchemaValidator:
         ItemCollection:
         'item-spec/json-schema/itemcollection.json',
         LabelItem:
-        'extensions/label/schema.json',
+        'extensions/label/json-schema/schema.json',
         EOItem:
         'extensions/eo/json-schema/schema.json',
 
         # TODO: Move off of custom schema once schema in spec is fixed.
         SingleFileSTAC: ('https://raw.githubusercontent.com/lossyrob/stac-spec/'
-                         '0.8.1/refactor-extension-schemas/extensions/'
-                         'single-file-stac/json-schema/single-file.json')
+                         '0.9.0/fix-single-file-stac-schema/extensions/'
+                         'single-file-stac/json-schema/schema.json')
     }
+    """
+    Schemas that need to be downloaded for caching that are not tied directly to STAC objects.
+    """
+    aux_schemas = [
+        'item-spec/json-schema/basics.json', 'item-spec/json-schema/datetimerange.json',
+        'item-spec/json-schema/instrument.json', 'item-spec/json-schema/licensing.json',
+        'item-spec/json-schema/metadata.json', 'item-spec/json-schema/provider.json',
+        'https://geojson.org/schema/Feature.json',
+        'https://geojson.org/schema/FeatureCollection.json'
+    ]
 
-    for c in schemas:
-        if not schemas[c].startswith('https'):
-            schemas[c] = '{}/{}'.format(SCHEMA_BASE_URI, schemas[c])
+    _schema_cache = {}
+
+    @staticmethod
+    def _abs_schema(uri):
+        return ('{}/{}'.format(SchemaValidator.SCHEMA_BASE_URI, uri)
+                if not uri.startswith('https') else uri)
+
+    _is_setup = False
+
+    @staticmethod
+    def _setup():
+        if not SchemaValidator._is_setup:
+            for c in SchemaValidator.schemas:
+                uri = SchemaValidator._abs_schema(SchemaValidator.schemas[c])
+                SchemaValidator.schemas[c] = uri
+                SchemaValidator._schema_cache[uri] = json.loads(STAC_IO.read_text(uri))
+
+            for uri in SchemaValidator.aux_schemas:
+                abs_uri = SchemaValidator._abs_schema(uri)
+                SchemaValidator._schema_cache[abs_uri] = json.loads(STAC_IO.read_text(abs_uri))
+
+            SchemaValidator._is_setup = True
 
     def __init__(self):
-        self.schema_cache = {}
+        SchemaValidator._setup()
 
     def get_schema(self, obj_type):
         schema_uri = SchemaValidator.schemas.get(obj_type)
 
         if schema_uri is None:
             raise Exception('No schema for type {}'.format(obj_type))
-        schema = self.schema_cache.get(obj_type)
+        schema = SchemaValidator._schema_cache.get(schema_uri)
         if schema is None:
-            schema = json.loads(STAC_IO.read_text(schema_uri))
-            self.schema_cache[obj_type] = schema
+            raise Exception('Could not read schema from {}'.format(schema_uri))
 
-        resolver = RefResolver(base_uri=schema_uri, referrer=schema)
+        resolver = RefResolver(base_uri=schema_uri,
+                               referrer=schema,
+                               store=SchemaValidator._schema_cache)
 
         return (schema, resolver)
 
