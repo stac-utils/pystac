7d7b63ffebc37b26f774e3d7811b0cc405c7fe7e
Simon Kassel <kassel.simon@gmail.com>
2020-06-09 11:55:15 -0400

    test bug fix

--- tests/test_item.py
+++ tests/test_item.py
@@ -50,3 +50,12 @@ class ItemTest(unittest.TestCase):
         assert len(item.assets) > 0
         for asset_key in item.assets:
             self.assertEqual(item.assets[asset_key].owner, item)
+
+    def test_self_contained_item(self):
+        m = TestCases.get_path('data-files/itemcollections/sample-item-collection.json')
+        with open(m) as f:
+            item_dict = json.load(f)['features'][0]
+        item_dict['links'] = [l for l in item_dict['links'] if l['rel'] == 'self']
+        item = Item.from_dict(item_dict)
+        self.assertIsInstance(item, Item)
+        self.assertEqual(len(item.links), 1)
