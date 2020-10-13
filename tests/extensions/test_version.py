91cf8e8a16e2a2274da59b1535d399acaf4213cb
Kurt Schwehr <schwehr@google.com>
2020-10-13 11:15:32 -0700

    test_version.py: Move validate out of tearDown and use snake_case
    Running validate in tearDown happens after the test results are
    recorded, so validate should happen in the tests.  This simplifies
    the tests where validate doesn't really make sense.
    
    Also removed a redundant call to self.collection.ext.enable.

--- tests/extensions/test_version.py
+++ tests/extensions/test_version.py
@@ -9,7 +9,7 @@ from pystac.extensions import version
 URL_TEMPLATE = 'http://example.com/catalog/%s.json'
 
 
-def MakeItem(year):
+def make_item(year):
     """Create basic test items that are only slightly different."""
     asset_id = f'USGS/GAP/CONUS/{year}'
     start = datetime.datetime(year, 1, 2)
@@ -27,86 +27,85 @@ class VersionItemExtTest(unittest.TestCase):
 
     def setUp(self):
         super().setUp()
-        self.item = MakeItem(2011)
+        self.item = make_item(2011)
 
         self.item.ext.enable(pystac.Extensions.VERSION)
 
-    def tearDown(self):
-        super().tearDown()
-        self.item.validate()
-
-    def testStacExtensions(self):
+    def test_stac_extensions(self):
         self.assertEqual([pystac.Extensions.VERSION], self.item.stac_extensions)
-        # Make sure the item is valid for tearDown check.
-        self.item.ext.version.apply(self.version)
 
-    def testAddVersion(self):
+    def test_add_version(self):
         self.item.ext.version.apply(self.version)
         self.assertEqual(self.version, self.item.ext.version.version)
         self.assertNotIn('deprecated', self.item.properties)
         self.assertFalse(self.item.ext.version.deprecated)
+        self.item.validate()
 
-    def testVersionInProperties(self):
+    def test_version_in_properties(self):
         self.item.ext.version.apply(self.version, deprecated=True)
         self.assertIn('version', self.item.properties)
         self.assertIn('deprecated', self.item.properties)
+        self.item.validate()
 
-    def testAddNotDeprecatedVersion(self):
+    def test_add_not_deprecated_version(self):
         self.item.ext.version.apply(self.version, deprecated=False)
         self.assertIn('deprecated', self.item.properties)
         self.assertFalse(self.item.ext.version.deprecated)
+        self.item.validate()
 
-    def testAddDeprecatedVersion(self):
+    def test_add_deprecated_version(self):
         self.item.ext.version.apply(self.version, deprecated=True)
         self.assertIn('deprecated', self.item.properties)
         self.assertTrue(self.item.ext.version.deprecated)
+        self.item.validate()
 
-    def testLatest(self):
+    def test_latest(self):
         year = 2013
-        latest = MakeItem(year)
+        latest = make_item(year)
         self.item.ext.version.apply(self.version, latest=latest)
         latest_link = self.item.ext.version.latest_link
         self.assertEqual(version.LATEST_VERSION, latest_link.rel)
 
         expected_href = URL_TEMPLATE % year
         self.assertEqual(expected_href, latest_link.get_href())
+        self.item.validate()
 
-    def testPredecessor(self):
+    def test_predecessor(self):
         year = 2010
-        predecessor = MakeItem(year)
+        predecessor = make_item(year)
         self.item.ext.version.apply(self.version, predecessor=predecessor)
         predecessor_link = self.item.ext.version.predecessor_link
         self.assertEqual(version.PREDECESSOR_VERSION, predecessor_link.rel)
 
         expected_href = URL_TEMPLATE % year
         self.assertEqual(expected_href, predecessor_link.get_href())
+        self.item.validate()
 
-    def testSuccessor(self):
+    def test_successor(self):
         year = 2012
-        successor = MakeItem(year)
+        successor = make_item(year)
         self.item.ext.version.apply(self.version, successor=successor)
         successor_link = self.item.ext.version.successor_link
         self.assertEqual(version.SUCCESSOR_VERSION, successor_link.rel)
 
         expected_href = URL_TEMPLATE % year
         self.assertEqual(expected_href, successor_link.get_href())
+        self.item.validate()
 
-    def testFailValidate(self):
+    def test_fail_validate(self):
         with self.assertRaises(pystac.validation.STACValidationError):
             self.item.validate()
 
-        self.item.ext.version.apply(self.version)
-
-    def testAllLinks(self):
+    def test_all_links(self):
         deprecated = True
-        latest = MakeItem(2013)
-        predecessor = MakeItem(2010)
-        successor = MakeItem(2012)
+        latest = make_item(2013)
+        predecessor = make_item(2010)
+        successor = make_item(2012)
         self.item.ext.version.apply(self.version, deprecated, latest, predecessor, successor)
-        # Just validate in tearDown.
+        self.item.validate()
 
 
-def MakeCollection(year):
+def make_collection(year):
     asset_id = 'my/collection/of/things'
     start = datetime.datetime(2014, 8, 10)
     end = datetime.datetime(year, 1, 3, 4, 5)
@@ -128,82 +127,80 @@ class VersionCollectionExtTest(unittest.TestCase):
 
     def setUp(self):
         super().setUp()
-        self.collection = MakeCollection(2011)
+        self.collection = make_collection(2011)
 
-        self.collection.ext.enable(pystac.Extensions.VERSION)
-
-    def tearDown(self):
-        super().tearDown()
-        self.collection.validate()
-
-    def testStacExtensions(self):
+    def test_stac_extensions(self):
         self.assertEqual([pystac.Extensions.VERSION], self.collection.stac_extensions)
-        # Make sure the Collection is valid for tearDown check.
-        self.collection.ext.version.apply(self.version)
 
-    def testAddVersion(self):
+    def test_add_version(self):
         self.collection.ext.version.apply(self.version)
         self.assertEqual(self.version, self.collection.ext.version.version)
         self.assertNotIn('deprecated', self.collection.extra_fields)
         self.assertFalse(self.collection.ext.version.deprecated)
+        self.collection.validate()
 
-    def testVersionDeprecated(self):
+    def test_version_deprecated(self):
         self.collection.ext.version.apply(self.version, deprecated=True)
         self.assertIn('version', self.collection.extra_fields)
         self.assertIn('deprecated', self.collection.extra_fields)
+        self.collection.validate()
 
-    def testAddNotDeprecatedVersion(self):
+    def test_add_not_deprecated_version(self):
         self.collection.ext.version.apply(self.version, deprecated=False)
         self.assertIn('deprecated', self.collection.extra_fields)
         self.assertFalse(self.collection.ext.version.deprecated)
+        self.collection.validate()
 
-    def testAddDeprecatedVersion(self):
+    def test_add_deprecated_version(self):
         self.collection.ext.version.apply(self.version, deprecated=True)
         self.assertIn('deprecated', self.collection.extra_fields)
         self.assertTrue(self.collection.ext.version.deprecated)
+        self.collection.validate()
 
-    def testLatest(self):
+    def test_latest(self):
         year = 2013
-        latest = MakeCollection(year)
+        latest = make_collection(year)
         self.collection.ext.version.apply(self.version, latest=latest)
         latest_link = self.collection.ext.version.latest_link
         self.assertEqual(version.LATEST_VERSION, latest_link.rel)
 
         expected_href = URL_TEMPLATE % year
         self.assertEqual(expected_href, latest_link.get_href())
+        self.collection.validate()
 
-    def testPredecessor(self):
+    def test_predecessor(self):
         year = 2010
-        predecessor = MakeCollection(year)
+        predecessor = make_collection(year)
         self.collection.ext.version.apply(self.version, predecessor=predecessor)
         predecessor_link = self.collection.ext.version.predecessor_link
         self.assertEqual(version.PREDECESSOR_VERSION, predecessor_link.rel)
 
         expected_href = URL_TEMPLATE % year
         self.assertEqual(expected_href, predecessor_link.get_href())
+        self.collection.validate()
 
-    def testSuccessor(self):
+    def test_successor(self):
         year = 2012
-        successor = MakeCollection(year)
+        successor = make_collection(year)
         self.collection.ext.version.apply(self.version, successor=successor)
         successor_link = self.collection.ext.version.successor_link
         self.assertEqual(version.SUCCESSOR_VERSION, successor_link.rel)
 
         expected_href = URL_TEMPLATE % year
         self.assertEqual(expected_href, successor_link.get_href())
+        self.collection.validate()
 
-    def testValidateMinimum(self):
+    def test_fail_validate(self):
         with self.assertRaises(pystac.validation.STACValidationError):
             self.collection.validate()
 
-        self.collection.ext.version.apply(self.version)
-
-    def testValidateAll(self):
+    def test_validate_all(self):
         deprecated = True
-        latest = MakeCollection(2013)
-        predecessor = MakeCollection(2010)
-        successor = MakeCollection(2012)
+        latest = make_collection(2013)
+        predecessor = make_collection(2010)
+        successor = make_collection(2012)
         self.collection.ext.version.apply(self.version, deprecated, latest, predecessor, successor)
+        self.collection.validate()
 
 
 if __name__ == '__main__':
