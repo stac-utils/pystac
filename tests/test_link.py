3bc9a01097f42c2b321279a078eb1105e557458e
Rob Emanuele <rdemanuele@gmail.com>
2020-10-21 19:28:18 -0400

    Modify test based on code review comments.
    - use consistent datetime
    - better docstring
    - use different ID and description
    - Get item link explicitly

--- tests/test_link.py
+++ tests/test_link.py
@@ -4,21 +4,21 @@ import unittest
 import pystac
 from tests.utils import (RANDOM_BBOX, RANDOM_GEOM)
 
+TEST_DATETIME = datetime(2020, 3, 14, 16, 32)
+
 
 class LinkTest(unittest.TestCase):
-    def test_link_doest_fail_if_href_is_none(self):
-        """Tests to cover a bug that was uncovered where a non-None HREF
-        was supposed"""
-        catalog = pystac.Catalog(id='test', description='test')
+    def test_link_does_not_fail_if_href_is_none(self):
+        """Test to ensure get_href does not fail when the href is None"""
+        catalog = pystac.Catalog(id='test', description='test desc')
         item = pystac.Item(id='test-item',
                            geometry=RANDOM_GEOM,
                            bbox=RANDOM_BBOX,
                            datetime=datetime.utcnow(),
-                           properties={'key': 'one'})
+                           properties={})
         catalog.add_item(item)
         catalog.set_self_href('/some/href')
         catalog.make_all_links_relative()
 
-        for link in catalog.links:
-            if link.rel == 'item':
-                self.assertIsNone(link.get_href())
+        link = catalog.get_single_link('item')
+        self.assertIsNone(link.get_href())
