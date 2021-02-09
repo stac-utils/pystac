b81363552f5dc535c6c02b51c7980f4aa6995522
Tyler Anderson <54593691+tyler-c2s@users.noreply.github.com>
2021-02-09 12:27:31 -0500

    add missing import

--- pystac/extensions/view.py
+++ pystac/extensions/view.py
@@ -1,6 +1,7 @@
 from pystac import Extensions
 from pystac.item import Item
 from pystac.extensions.base import (ItemExtension, ExtensionDefinition, ExtendedObject)
+from typing import Optional
 
 
 class ViewItemExt(ItemExtension):
