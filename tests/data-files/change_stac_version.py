8b873ad6f99116fedf96c44bf20c9cfae0d4a0d4
Rob Emanuele <rdemanuele@gmail.com>
2021-04-27 00:14:37 -0400

    Modify change_stac_version to perform migration

--- tests/data-files/change_stac_version.py
+++ tests/data-files/change_stac_version.py
@@ -3,34 +3,59 @@ Script to change the version property of the test files for PySTAC.
 This is used when upgrading to a new version of STAC.
 """
 import os
+import re
 import argparse
 import json
 
+import pystac as ps
+
+TARGET_VERSION = ps.get_stac_version()
+
+def migrate(path: str) -> None:
+    try:
+        with open(path) as f:
+            stac_json = json.load(f)
+    except json.decoder.JSONDecodeError:
+        print(f'Cannot read {path}')
+        raise
+
+    if 'stac_version' in stac_json:
+        cur_ver = stac_json['stac_version']
+        if not cur_ver == TARGET_VERSION:
+            print('  - Migrating {} from {} to {}...'.format(
+                path, cur_ver, TARGET_VERSION))
+            obj = ps.read_dict(stac_json)
+            migrated = obj.to_dict()
+            with open(path, 'w') as f:
+                json.dump(migrated, f, indent=2)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('stac_version',
-                        metavar='STAC_VERSION',
-                        help='The STAC_VERSION to ensure all STAC objects are using.')
+    parser.add_argument('--file',
+                        metavar='FILE',
+                        help='Only migrate this specific file.')
 
     args = parser.parse_args()
 
-    data_files_dir = os.path.dirname(os.path.realpath(__file__))
+    if args.file:
+        migrate(os.path.abspath(args.file))
+    else:
 
-    # Skip examples directory, which contains version specific STACs...
-    dirs_to_check = [x for x in os.listdir(data_files_dir) if x != 'examples']
+        data_files_dir = os.path.dirname(os.path.realpath(__file__))
 
-    for d in dirs_to_check:
-        for root, _, files in os.walk(d):
-            for fname in files:
-                if fname.endswith('.json'):
-                    path = os.path.join(root, fname)
-                    with open(path) as f:
-                        stac_json = json.load(f)
-                    if 'stac_version' in stac_json:
-                        cur_ver = stac_json['stac_version']
-                        if not cur_ver == args.stac_version:
-                            print('Changing {} version from {} to {}...'.format(
-                                fname, cur_ver, args.stac_version))
-                            stac_json['stac_version'] = args.stac_version
-                            with open(path, 'w') as f:
-                                json.dump(stac_json, f, indent=2)
+        # Skip examples directory, which contains version specific STACs...
+        dirs_to_check = [
+            os.path.join(data_files_dir, x) for x in os.listdir(data_files_dir) if x != 'examples'
+        ]
+
+        for d in dirs_to_check:
+            print(f'checking in {d}..')
+            for root, _, files in os.walk(d):
+                # Skip directories with a version tag
+                if re.match(r".*-v\d.*", root):
+                    continue
+                for fname in files:
+                    if fname.endswith('.json'):
+                        path = os.path.join(root, fname)
+                        migrate(path)
