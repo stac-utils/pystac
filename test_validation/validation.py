a345f0549dda700762ee25ee1710e14b7f0c4684
jonhealy1 <jonathan.d.healy@gmail.com>
2021-05-07 17:09:07 -0700

    fix 1rc1 extensions and 1beta1 schemas

--- test_validation/validation.py
+++
@@ -1,7 +0,0 @@
-import json
-from pystac.validation import validate_dict
-
-with open('../test_validation/test_data/1rc2/extended-item.json') as f:
-    js = json.load(f)
-result = validate_dict(js)
-print(sorted(result))
