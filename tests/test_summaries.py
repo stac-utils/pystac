af126fb019687bccd8c34c6160b81906cd8b55d3
Rob Emanuele <rdemanuele@gmail.com>
2021-05-18 01:35:06 -0400

    test_case_5 is not a collection; use items from catalog in test

--- tests/test_summaries.py
+++ tests/test_summaries.py
@@ -7,14 +7,14 @@ from tests.utils import TestCases
 class SummariesTest(unittest.TestCase):
     def test_summary(self):
         coll = TestCases.test_case_5()
-        summaries = Summarizer().summarize(coll)
+        summaries = Summarizer().summarize(coll.get_all_items())
         summaries_dict = summaries.to_dict()
         self.assertEquals(len(summaries_dict["eo:bands"]), 4)
         self.assertEquals(len(summaries_dict["proj:epsg"]), 1)
 
     def test_summary_limit(self):
         coll = TestCases.test_case_5()
-        summaries = Summarizer().summarize(coll)
+        summaries = Summarizer().summarize(coll.get_all_items())
         summaries.maxcount = 2
         summaries_dict = summaries.to_dict()
         self.assertIsNone(summaries_dict.get("eo:bands"))
@@ -23,14 +23,14 @@ class SummariesTest(unittest.TestCase):
     def test_summary_custom_fields_file(self):
         coll = TestCases.test_case_5()
         path = TestCases.get_path("data-files/summaries/fields_no_bands.json")
-        summaries = Summarizer(path).summarize(coll)
+        summaries = Summarizer(path).summarize(coll.get_all_items())
         summaries_dict = summaries.to_dict()
         self.assertIsNone(summaries_dict.get("eo:bands"))
         self.assertEquals(len(summaries_dict["proj:epsg"]), 1)
 
     def test_summary_wrong_custom_fields_file(self):
         coll = TestCases.test_case_5()
-        summaries = Summarizer("wrong/path").summarize(coll)
+        summaries = Summarizer("wrong/path").summarize(coll.get_all_items())
         summaries_dict = summaries.to_dict()
         self.assertEquals(len(summaries_dict["eo:bands"]), 4)
         self.assertEquals(len(summaries_dict["proj:epsg"]), 1)
@@ -41,5 +41,5 @@ class SummariesTest(unittest.TestCase):
 
     def test_summary_not_empty(self):
         coll = TestCases.test_case_5()
-        summaries = Summarizer().summarize(coll)
+        summaries = Summarizer().summarize(coll.get_all_items())
         self.assertFalse(summaries.is_empty())
