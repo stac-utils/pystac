ca0a6fad826313ba8ed2edb2d6ae99df5d541550
Jon Duckworth <duckontheweb@gmail.com>
2022-02-08 14:23:47 -0500

    Configure asv and add import benchmark

---
+++ benchmarks/benchmarks/import_pystac.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+
+class ImportPySTAC:
+    def setup(self, *args: Any, **kwargs: Any) -> None:
+        def import_pystac() -> None:
+            import pystac  # noqa: F401
+
+        self._import_pystac = import_pystac
+
+    def time_import_pystac(self) -> None:
+        self._import_pystac()
