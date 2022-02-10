3f5bb3d338152920595f05a787cbe0a1c8cc799d
Jon Duckworth <duckontheweb@gmail.com>
2022-02-10 12:59:33 -0500

    Add convenience script for running locally

--- benchmarks/benchmarks/_util.py
+++ benchmarks/benchmarks/_util.py
@@ -1,15 +1,12 @@
 import os
+from typing import Union
 
-
-def get_data_path(rel_path: os.PathLike) -> str:
+def get_data_path(rel_path: Union[str, os.PathLike]) -> str:
     """Gets the absolute path to a file based on a path relative to the
     tests/data-files directory in this repo."""
     rel_path = os.fspath(rel_path)
-    return os.path.abspath(os.path.join(
-        os.path.dirname(__file__), 
-        "..", 
-        "..",
-        "tests",
-        "data-files",
-        rel_path
-    ))
+    return os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__), "..", "..", "tests", "data-files", rel_path
+        )
+    )
