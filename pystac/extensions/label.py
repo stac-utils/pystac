ccf12d26a3af3a3ef409b41eb5113c03aa416e06
Jon Duckworth <duckontheweb@gmail.com>
2022-02-22 10:57:13 -0500

    Update to Label Extension v1.0.1 (#726)
    * Update to Label Extension v1.0.1
    
    * Add CHANGELOG entry for #726

--- pystac/extensions/label.py
+++ pystac/extensions/label.py
@@ -11,7 +11,7 @@ from pystac.serialization.identify import STACJSONDescription, STACVersionID
 from pystac.extensions.hooks import ExtensionHooks
 from pystac.utils import StringEnum, get_required, map_opt
 
-SCHEMA_URI = "https://stac-extensions.github.io/label/v1.0.0/schema.json"
+SCHEMA_URI = "https://stac-extensions.github.io/label/v1.0.1/schema.json"
 
 PREFIX = "label:"
 
@@ -791,7 +791,10 @@ class SummariesLabelExtension(SummariesExtension):
 
 class LabelExtensionHooks(ExtensionHooks):
     schema_uri: str = SCHEMA_URI
-    prev_extension_ids = {"label"}
+    prev_extension_ids = {
+        "label",
+        "https://stac-extensions.github.io/label/v1.0.0/schema.json",
+    }
     stac_object_types = {pystac.STACObjectType.ITEM}
 
     def get_object_links(
