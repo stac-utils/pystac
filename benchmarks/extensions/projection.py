1afea50825a89b9a1b0e1322390d8db3139bd766
Pete Gadomski <pete.gadomski@gmail.com>
2023-01-09 15:37:07 -0700

    feat: add projection benchmarks
    I'm not really sure how useful this is, but it was asked for so at least we have
    something.

---
+++ benchmarks/extensions/projection.py
@@ -0,0 +1,14 @@
+import datetime
+
+from pystac import Item
+from pystac.extensions.projection import ProjectionExtension
+
+from .._base import Bench
+
+
+class ProjectionBench(Bench):
+    def setup(self) -> None:
+        self.item = Item("an-id", None, None, datetime.datetime.now(), {})
+
+    def time_add_projection_extension(self) -> None:
+        _ = ProjectionExtension.ext(self.item, add_if_missing=True)
