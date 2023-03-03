b8071acf9ee959dfd03c793c660443180efc4e06
Pete Gadomski <pete.gadomski@gmail.com>
2023-03-03 09:27:40 -0700

    docs: remove customizations
    These were breaking layouts with the pydata_sphinx_theme, and (to me) the
    defaults look fine? I am not a designer, though.

--- docs/conf.py
+++ docs/conf.py
@@ -74,10 +74,10 @@ extlinks = {
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+# templates_path = ["_templates"]
 
 # Static CSS files
-html_css_files = ["custom.css"]
+# html_css_files = ["custom.css"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
