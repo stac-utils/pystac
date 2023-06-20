96af967d3a0368ee297ec76a52a1f5750ffd083e
Julia Signell <jsignell@gmail.com>
2023-06-20 10:19:35 -0400

    Switch to importlib for path and remove extensions

--- pystac/validation/local_validator.py
+++ pystac/validation/local_validator.py
@@ -1,16 +1,11 @@
+import importlib
 import json
-from pathlib import Path
 from typing import Any, Dict, List, cast
 
 from jsonschema import Draft7Validator, RefResolver, ValidationError
 
 from pystac.errors import STACLocalValidationError
-from pystac.extensions.eo import SCHEMA_URI as EO_SCHEMA_URI
-from pystac.extensions.label import SCHEMA_URI as LABEL_SCHEMA_URI
-from pystac.extensions.view import SCHEMA_URI as VIEW_SCHEMA_URI
 from pystac.version import STACVersion
-
-here = Path(__file__).resolve().parent
 
 VERSION = STACVersion.DEFAULT_STAC_VERSION
 ITEM_SCHEMA_URI = (
@@ -23,12 +18,6 @@ CATALOG_SCHEMA_URI = (
 
 
 class LocalValidator:
-    extension_schema_uris: List[str] = [
-        EO_SCHEMA_URI,
-        LABEL_SCHEMA_URI,
-        VIEW_SCHEMA_URI,
-    ]
-
     def _validate_from_local(
         self, schema_uri: str, stac_dict: Dict[str, Any]
     ) -> List[ValidationError]:
@@ -38,21 +27,11 @@ class LocalValidator:
             validator = self.collection_validator(VERSION)
         elif schema_uri == CATALOG_SCHEMA_URI:
             validator = self.catalog_validator(VERSION)
-        elif schema_uri in self.extension_schema_uris:
-            validator = self.extension_validator(schema_uri)
         else:
             raise STACLocalValidationError(
                 f"Schema not available locally: {schema_uri}"
             )
         return list(validator.iter_errors(stac_dict))
-
-    @staticmethod
-    def extension_validator(schema_uri: str) -> Draft7Validator:
-        local_path = schema_uri.replace(
-            "https://stac-extensions.github.io", "extensions"
-        )
-        schema = _read_schema(local_path)
-        return Draft7Validator(schema)
 
     @staticmethod
     def catalog_validator(version: str = VERSION) -> Draft7Validator:
@@ -84,5 +63,7 @@ class LocalValidator:
 
 
 def _read_schema(file_name: str) -> Dict[str, Any]:
-    with (here / "jsonschemas" / file_name).open("r") as f:
+    with importlib.resources.files("pystac.validation.jsonschemas").joinpath(
+        file_name
+    ).open("r") as f:
         return cast(Dict[str, Any], json.load(f))
