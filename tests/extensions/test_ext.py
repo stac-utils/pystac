d2263b0e156c07a7c072027d13a3de9e60c79c65
Pete Gadomski <pete.gadomski@gmail.com>
2023-10-13 13:29:12 -0400

    refactor: test import naming, ext

--- tests/extensions/test_ext.py
+++ tests/extensions/test_ext.py
@@ -2,7 +2,7 @@ import logging
 
 import pytest
 
-import pystac
+from pystac import Asset, Collection, Item
 from pystac.errors import ExtensionNotImplemented
 from pystac.extensions.ext import (
     EXTENSION_NAME_MAPPING,
@@ -18,12 +18,12 @@ logger = logging.getLogger()
 
 
 @pytest.fixture
-def eo_ext_item() -> pystac.Item:
+def eo_ext_item() -> Item:
     ext_item_uri = get_data_file("eo/eo-landsat-example.json")
-    return pystac.Item.from_file(ext_item_uri)
+    return Item.from_file(ext_item_uri)
 
 
-def test_ext_syntax_has(eo_ext_item: pystac.Item) -> None:
+def test_ext_syntax_has(eo_ext_item: Item) -> None:
     assert eo_ext_item.ext.has("eo") is True
     assert eo_ext_item.ext.has("proj") is False
 
@@ -31,22 +31,22 @@ def test_ext_syntax_has(eo_ext_item: pystac.Item) -> None:
     assert eo_ext_item.assets["B1"].ext.has("proj") is False
 
 
-def test_ext_syntax_raises_if_ext_not_on_obj(eo_ext_item: pystac.Item) -> None:
+def test_ext_syntax_raises_if_ext_not_on_obj(eo_ext_item: Item) -> None:
     with pytest.raises(ExtensionNotImplemented):
         eo_ext_item.ext.proj.epsg
 
 
-def test_ext_syntax_ext_can_be_added(eo_ext_item: pystac.Item) -> None:
+def test_ext_syntax_ext_can_be_added(eo_ext_item: Item) -> None:
     eo_ext_item.ext.add("proj")
     assert eo_ext_item.ext.proj.epsg is None
 
 
-def test_ext_syntax_trying_to_add_invalid_ext_raises(item: pystac.Item) -> None:
+def test_ext_syntax_trying_to_add_invalid_ext_raises(item: Item) -> None:
     with pytest.raises(KeyError, match="Extension 'foo' is not a valid extension"):
         item.ext.add("foo")  # type: ignore
 
 
-def test_ext_syntax_ext_can_be_removed(eo_ext_item: pystac.Item) -> None:
+def test_ext_syntax_ext_can_be_removed(eo_ext_item: Item) -> None:
     original_n = len(eo_ext_item.stac_extensions)
     eo_ext_item.ext.remove("eo")
     with pytest.raises(
@@ -75,7 +75,7 @@ all_collection_ext_props = {a for a in dir(CollectionExt) if not a.startswith("_
 
 @pytest.mark.parametrize("name", all_asset_ext_props)
 def test_ext_syntax_every_prop_can_be_added_to_asset(
-    asset: pystac.Asset, name: EXTENSION_NAMES
+    asset: Asset, name: EXTENSION_NAMES
 ) -> None:
     assert asset.ext.has(name) is False
     asset.ext.add(name)
@@ -89,7 +89,7 @@ def test_ext_syntax_every_prop_can_be_added_to_asset(
 
 @pytest.mark.parametrize("name", all_item_ext_props)
 def test_ext_syntax_every_prop_can_be_added_to_item(
-    item: pystac.Item, name: EXTENSION_NAMES
+    item: Item, name: EXTENSION_NAMES
 ) -> None:
     assert item.ext.has(name) is False
     item.ext.add(name)
@@ -103,7 +103,7 @@ def test_ext_syntax_every_prop_can_be_added_to_item(
 
 @pytest.mark.parametrize("name", all_collection_ext_props)
 def test_ext_syntax_every_prop_can_be_added_to_collection(
-    collection: pystac.Collection, name: EXTENSION_NAMES
+    collection: Collection, name: EXTENSION_NAMES
 ) -> None:
     assert collection.ext.has(name) is False
     collection.ext.add(name)
