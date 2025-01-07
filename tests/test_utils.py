7173abdbab0f5dd55ea47ce38bb4564c5e40a4db
Julia Signell <jsignell@gmail.com>
2025-01-07 11:32:57 -0500

    Try to fix windows

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -177,7 +177,11 @@ class UtilsTest(unittest.TestCase):
 
         for source_href, start_href, expected in test_cases:
             actual = make_absolute_href(source_href, start_href)
-            _, actual = os.path.splitdrive(actual)
+            if expected.startswith("file://"):
+                _, actual = os.path.splitdrive(actual.replace("file://", ""))
+                actual = f"file://{actual}"
+            else:
+                _, actual = os.path.splitdrive(actual)
             self.assertEqual(actual, expected)
 
     def test_make_absolute_href_on_vsitar(self) -> None:
