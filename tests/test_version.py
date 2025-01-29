65857a21371b4ff4953157fa4632a1c7039fb2b4
teks <tom.olson@gmail.com>
2025-01-29 10:35:25 -0500

    993 convert test_version.py to pytest

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,25 +1,31 @@
 import os
-import unittest
+from collections.abc import Generator
 from unittest.mock import patch
+
+import pytest
 
 import pystac
 from tests.utils import TestCases
 
 
-class VersionTest(unittest.TestCase):
-    def setUp(self) -> None:
-        pystac.version.STACVersion._override_version = None
-
-    def test_override_stac_version_with_environ(self) -> None:
-        override_version = "1.0.0-gamma.2"
-        with patch.dict(os.environ, {"PYSTAC_STAC_VERSION_OVERRIDE": override_version}):
-            cat = TestCases.case_1()
-            d = cat.to_dict()
-        self.assertEqual(d["stac_version"], override_version)
-
-    def test_override_stac_version_with_call(self) -> None:
-        override_version = "1.0.0-delta.2"
-        pystac.set_stac_version(override_version)
+def test_override_stac_version_with_environ() -> None:
+    override_version = "1.0.0-gamma.2"
+    with patch.dict(os.environ, {"PYSTAC_STAC_VERSION_OVERRIDE": override_version}):
         cat = TestCases.case_1()
         d = cat.to_dict()
-        self.assertEqual(d["stac_version"], override_version)
+    assert d["stac_version"] == override_version
+
+
+@pytest.fixture
+def override_pystac_version() -> Generator[str]:
+    stac_version = pystac.get_stac_version()
+    override_version = "1.0.0-delta.2"
+    pystac.set_stac_version(override_version)
+    yield override_version
+    pystac.set_stac_version(stac_version)
+
+
+def test_override_stac_version_with_call(override_pystac_version: str) -> None:
+    cat = TestCases.case_1()
+    d = cat.to_dict()
+    assert d["stac_version"] == override_pystac_version
