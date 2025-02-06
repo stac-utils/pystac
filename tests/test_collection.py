d2806c545c792eacf124296650c53a8786eb692b
teks <tom.olson@gmail.com>
2025-02-05 19:26:47 -0500

    993 complete test_collection.py conversion to pytest

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import json
 import os
 import tempfile
-import unittest
 from collections.abc import Iterator
 from copy import deepcopy
 from datetime import datetime
