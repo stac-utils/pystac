332093cbd97bd8fc034bd96e6a4a8a6c8be52bd9
teks <tom.olson@gmail.com>
2025-02-18 09:59:43 -0500

    993 finalize test_cache.py w/ pre-commit

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,4 +1,3 @@
-import unittest
 from typing import Any
 
 import pystac
@@ -24,6 +23,7 @@ def test_ResolvedObjectCache_get_or_cache_returns_previously_cached_href() -> No
     identical_cat = create_catalog(1)
     cache_result_2 = cache.get_or_cache(identical_cat)
     assert cache_result_2 is cat
+
 
 def test_ResolvedObjectCache_get_or_cache_returns_previously_cached_id() -> None:
     cache = ResolvedObjectCache()
@@ -60,14 +60,15 @@ def test_ResolvedObjectCollectionCache_merge() -> None:
         ResolvedObjectCache(), cached_ids=cached_ids_2, cached_hrefs=cached_hrefs_2
     )
 
-    merged = ResolvedObjectCollectionCache.merge(
-        ResolvedObjectCache(), cache1, cache2
-    )
+    merged = ResolvedObjectCollectionCache.merge(ResolvedObjectCache(), cache1, cache2)
 
     assert set(merged.cached_ids.keys()) == {cat.id for cat in [cat1, cat3]}
     assert merged.get_by_id(cat1.id) is cat1
-    assert set(merged.cached_hrefs.keys()) == {cat.get_self_href() for cat in [cat2, cat4]}
+    assert set(merged.cached_hrefs.keys()) == {
+        cat.get_self_href() for cat in [cat2, cat4]
+    }
     assert merged.get_by_href(get_opt(cat2.get_self_href())) is cat2
+
 
 def test_ResolvedObjectCollectionCache_cache() -> None:
     cache = ResolvedObjectCache().as_collection_cache()
