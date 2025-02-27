2d2eb3a0b320c7a721294b82175d9ec732c53bd2
teks <tom.olson@gmail.com>
2025-02-27 14:33:27 -0500

    993 pre-commit to finish test_sar.py

--- tests/extensions/test_sar.py
+++ tests/extensions/test_sar.py
@@ -1,6 +1,5 @@
 """Tests for pystac.extensions.sar."""
 
-import unittest
 from datetime import datetime
 from random import choice
 from string import ascii_letters
@@ -35,11 +34,13 @@ def item() -> pystac.Item:
 def sentinel_item() -> pystac.Item:
     return pystac.Item.from_file(TestCases.get_path("data-files/sar/sentinel-1.json"))
 
+
 @pytest.fixture
 def collection() -> pystac.Collection:
     return pystac.Collection.from_file(
         TestCases.get_path("data-files/collections/multi-extent.json")
     )
+
 
 def test_stac_extensions(item: pystac.Item) -> None:
     assert SarExtension.has_extension(item)
@@ -55,9 +56,7 @@ def test_required(item: pystac.Item) -> None:
     ]
     product_type: str = "Some product"
 
-    SarExtension.ext(item).apply(
-        mode, frequency_band, polarizations, product_type
-    )
+    SarExtension.ext(item).apply(mode, frequency_band, polarizations, product_type)
     assert mode == SarExtension.ext(item).instrument_mode
     assert sar.INSTRUMENT_MODE_PROP in item.properties
 
@@ -153,7 +152,9 @@ def test_polarization_must_be_list(item: pystac.Item) -> None:
         )
 
 
-def test_should_return_none_when_observation_direction_is_not_set(item: pystac.Item) -> None:
+def test_should_return_none_when_observation_direction_is_not_set(
+    item: pystac.Item,
+) -> None:
     extension = SarExtension.ext(item)
     extension.apply(
         choice(ascii_letters),
@@ -181,6 +182,7 @@ def test_extension_not_implemented(sentinel_item: pystac.Item) -> None:
     ownerless_asset = pystac.Asset.from_dict(asset.to_dict())
     _ = SarExtension.ext(ownerless_asset)
 
+
 def test_item_ext_add_to(sentinel_item: pystac.Item) -> None:
     sentinel_item.stac_extensions.remove(SarExtension.get_schema_uri())
     assert SarExtension.get_schema_uri() not in sentinel_item.stac_extensions
@@ -188,6 +190,7 @@ def test_item_ext_add_to(sentinel_item: pystac.Item) -> None:
     _ = SarExtension.ext(sentinel_item, add_if_missing=True)
 
     assert SarExtension.get_schema_uri() in sentinel_item.stac_extensions
+
 
 def test_asset_ext_add_to(sentinel_item: pystac.Item) -> None:
     sentinel_item.stac_extensions.remove(SarExtension.get_schema_uri())
@@ -198,11 +201,13 @@ def test_asset_ext_add_to(sentinel_item: pystac.Item) -> None:
 
     assert SarExtension.get_schema_uri() in sentinel_item.stac_extensions
 
+
 def test_should_raise_exception_when_passing_invalid_extension_object() -> None:
     with pytest.raises(
-        ExtensionTypeError, match=r"^SarExtension does not apply to type 'object'$",
+        ExtensionTypeError,
+        match=r"^SarExtension does not apply to type 'object'$",
     ):
-        SarExtension.ext(object()) # type: ignore
+        SarExtension.ext(object())  # type: ignore
 
 
 def test_summaries_instrument_mode(collection: pystac.Collection) -> None:
@@ -211,10 +216,11 @@ def test_summaries_instrument_mode(collection: pystac.Collection) -> None:
 
     summaries_ext.instrument_mode = instrument_mode_list
 
-    assert  summaries_ext.instrument_mode == instrument_mode_list 
+    assert summaries_ext.instrument_mode == instrument_mode_list
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:instrument_mode"] == instrument_mode_list 
+    assert summaries_dict["sar:instrument_mode"] == instrument_mode_list
+
 
 def test_summaries_frequency_band(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -222,10 +228,11 @@ def test_summaries_frequency_band(collection: pystac.Collection) -> None:
 
     summaries_ext.frequency_band = frequency_band_list
 
-    assert  summaries_ext.frequency_band == frequency_band_list 
+    assert summaries_ext.frequency_band == frequency_band_list
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:frequency_band"] == frequency_band_list 
+    assert summaries_dict["sar:frequency_band"] == frequency_band_list
+
 
 def test_summaries_polarizations(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -233,11 +240,12 @@ def test_summaries_polarizations(collection: pystac.Collection) -> None:
 
     summaries_ext.polarizations = polarizations_list
 
-    assert  summaries_ext.polarizations == polarizations_list 
+    assert summaries_ext.polarizations == polarizations_list
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:polarizations"] == polarizations_list 
+    assert summaries_dict["sar:polarizations"] == polarizations_list
+
 
 def test_summaries_product_type(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -245,11 +253,12 @@ def test_summaries_product_type(collection: pystac.Collection) -> None:
 
     summaries_ext.product_type = product_type_list
 
-    assert  summaries_ext.product_type == product_type_list 
+    assert summaries_ext.product_type == product_type_list
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:product_type"] == product_type_list 
+    assert summaries_dict["sar:product_type"] == product_type_list
+
 
 def test_summaries_center_frequency(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -257,11 +266,12 @@ def test_summaries_center_frequency(collection: pystac.Collection) -> None:
 
     summaries_ext.center_frequency = center_frequency_range
 
-    assert  summaries_ext.center_frequency == center_frequency_range 
+    assert summaries_ext.center_frequency == center_frequency_range
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:center_frequency"] == center_frequency_range.to_dict() 
+    assert summaries_dict["sar:center_frequency"] == center_frequency_range.to_dict()
+
 
 def test_summaries_resolution_range(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -269,11 +279,12 @@ def test_summaries_resolution_range(collection: pystac.Collection) -> None:
 
     summaries_ext.resolution_range = resolution_range_range
 
-    assert  summaries_ext.resolution_range == resolution_range_range 
+    assert summaries_ext.resolution_range == resolution_range_range
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:resolution_range"] == resolution_range_range.to_dict() 
+    assert summaries_dict["sar:resolution_range"] == resolution_range_range.to_dict()
+
 
 def test_summaries_resolution_azimuth(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -281,11 +292,14 @@ def test_summaries_resolution_azimuth(collection: pystac.Collection) -> None:
 
     summaries_ext.resolution_azimuth = resolution_azimuth_range
 
-    assert  summaries_ext.resolution_azimuth == resolution_azimuth_range 
+    assert summaries_ext.resolution_azimuth == resolution_azimuth_range
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:resolution_azimuth"] == resolution_azimuth_range.to_dict() 
+    assert (
+        summaries_dict["sar:resolution_azimuth"] == resolution_azimuth_range.to_dict()
+    )
+
 
 def test_summaries_pixel_spacing_range(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -293,11 +307,14 @@ def test_summaries_pixel_spacing_range(collection: pystac.Collection) -> None:
 
     summaries_ext.pixel_spacing_range = pixel_spacing_range_range
 
-    assert  summaries_ext.pixel_spacing_range == pixel_spacing_range_range 
+    assert summaries_ext.pixel_spacing_range == pixel_spacing_range_range
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:pixel_spacing_range"] == pixel_spacing_range_range.to_dict() 
+    assert (
+        summaries_dict["sar:pixel_spacing_range"] == pixel_spacing_range_range.to_dict()
+    )
+
 
 def test_summaries_pixel_spacing_azimuth(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -305,11 +322,15 @@ def test_summaries_pixel_spacing_azimuth(collection: pystac.Collection) -> None:
 
     summaries_ext.pixel_spacing_azimuth = pixel_spacing_azimuth_range
 
-    assert  summaries_ext.pixel_spacing_azimuth == pixel_spacing_azimuth_range 
+    assert summaries_ext.pixel_spacing_azimuth == pixel_spacing_azimuth_range
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:pixel_spacing_azimuth"] == pixel_spacing_azimuth_range.to_dict() 
+    assert (
+        summaries_dict["sar:pixel_spacing_azimuth"]
+        == pixel_spacing_azimuth_range.to_dict()
+    )
+
 
 def test_summaries_looks_range(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -317,11 +338,12 @@ def test_summaries_looks_range(collection: pystac.Collection) -> None:
 
     summaries_ext.looks_range = looks_range_range
 
-    assert  summaries_ext.looks_range == looks_range_range 
+    assert summaries_ext.looks_range == looks_range_range
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:looks_range"] == looks_range_range.to_dict() 
+    assert summaries_dict["sar:looks_range"] == looks_range_range.to_dict()
+
 
 def test_summaries_looks_azimuth(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -329,11 +351,12 @@ def test_summaries_looks_azimuth(collection: pystac.Collection) -> None:
 
     summaries_ext.looks_azimuth = looks_azimuth_range
 
-    assert  summaries_ext.looks_azimuth == looks_azimuth_range 
+    assert summaries_ext.looks_azimuth == looks_azimuth_range
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:looks_azimuth"] == looks_azimuth_range.to_dict() 
+    assert summaries_dict["sar:looks_azimuth"] == looks_azimuth_range.to_dict()
+
 
 def test_summaries_looks_equivalent_number(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -341,11 +364,15 @@ def test_summaries_looks_equivalent_number(collection: pystac.Collection) -> Non
 
     summaries_ext.looks_equivalent_number = looks_equivalent_number_range
 
-    assert  summaries_ext.looks_equivalent_number == looks_equivalent_number_range 
+    assert summaries_ext.looks_equivalent_number == looks_equivalent_number_range
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:looks_equivalent_number"] == looks_equivalent_number_range.to_dict() 
+    assert (
+        summaries_dict["sar:looks_equivalent_number"]
+        == looks_equivalent_number_range.to_dict()
+    )
+
 
 def test_summaries_observation_direction(collection: pystac.Collection) -> None:
     summaries_ext = SarExtension.summaries(collection, True)
@@ -353,8 +380,8 @@ def test_summaries_observation_direction(collection: pystac.Collection) -> None:
 
     summaries_ext.observation_direction = observation_direction_list
 
-    assert  summaries_ext.observation_direction == observation_direction_list 
+    assert summaries_ext.observation_direction == observation_direction_list
 
     summaries_dict = collection.to_dict()["summaries"]
 
-    assert  summaries_dict["sar:observation_direction"] == observation_direction_list 
+    assert summaries_dict["sar:observation_direction"] == observation_direction_list
