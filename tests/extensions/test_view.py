2787700f42ee86e3430259792d08edb6a1cfca9f
teks <tom.olson@gmail.com>
2025-03-05 09:49:43 -0500

    993 naturally ruff has the last word

--- tests/extensions/test_view.py
+++ tests/extensions/test_view.py
@@ -1,5 +1,4 @@
 import json
-import unittest
 
 import pytest
 
@@ -10,8 +9,8 @@ from pystac.extensions.view import ViewExtension
 from pystac.summaries import RangeSummary
 from tests.utils import TestCases, assert_to_from_dict
 
-
 EXAMPLE_URI = TestCases.get_path("data-files/view/example-landsat8.json")
+
 
 @pytest.fixture
 def item() -> Item:
@@ -20,9 +19,9 @@ def item() -> Item:
 
 @pytest.fixture
 def collection() -> Collection:
-    return Collection.from_file( TestCases.get_path(
-        "data-files/view/collection-with-summaries.json"
-    ))
+    return Collection.from_file(
+        TestCases.get_path("data-files/view/collection-with-summaries.json")
+    )
 
 
 def test_to_from_dict() -> None:
@@ -50,10 +49,12 @@ def test_apply() -> None:
     assert ViewExtension.ext(item).sun_azimuth == 4.0
     assert ViewExtension.ext(item).sun_elevation == 5.0
 
+
 @pytest.mark.vcr()
 def test_validate_view(item: Item) -> None:
     assert ViewExtension.has_extension(item)
     item.validate()
+
 
 @pytest.mark.vcr()
 def test_off_nadir(item: Item) -> None:
@@ -70,17 +71,22 @@ def test_off_nadir(item: Item) -> None:
     # Get from Asset
     asset_no_prop = item.assets["blue"]
     asset_prop = item.assets["red"]
-    assert  ViewExtension.ext(asset_no_prop).off_nadir == ViewExtension.ext(item).off_nadir
+    assert (
+        ViewExtension.ext(asset_no_prop).off_nadir == ViewExtension.ext(item).off_nadir
+    )
     assert ViewExtension.ext(asset_prop).off_nadir == 3.0
 
     # Set to Asset
     asset_value = 13.0
     ViewExtension.ext(asset_no_prop).off_nadir = asset_value
-    assert  ViewExtension.ext(asset_no_prop).off_nadir != ViewExtension.ext(item).off_nadir
+    assert (
+        ViewExtension.ext(asset_no_prop).off_nadir != ViewExtension.ext(item).off_nadir
+    )
     assert ViewExtension.ext(asset_no_prop).off_nadir == asset_value
 
     # Validate
     item.validate()
+
 
 @pytest.mark.vcr()
 def test_incidence_angle(item: Item) -> None:
@@ -88,24 +94,31 @@ def test_incidence_angle(item: Item) -> None:
     assert "view:incidence_angle" in item.properties
     view_incidence_angle = ViewExtension.ext(item).incidence_angle
     assert view_incidence_angle is not None
-    assert  view_incidence_angle == item.properties["view:incidence_angle"]
+    assert view_incidence_angle == item.properties["view:incidence_angle"]
     # Set
     ViewExtension.ext(item).incidence_angle = view_incidence_angle + 10
-    assert  view_incidence_angle + 10 == item.properties["view:incidence_angle"]
+    assert view_incidence_angle + 10 == item.properties["view:incidence_angle"]
     # Get from Asset
     asset_no_prop = item.assets["blue"]
     asset_prop = item.assets["red"]
-    assert  ViewExtension.ext(asset_no_prop).incidence_angle == ViewExtension.ext(item).incidence_angle
+    assert (
+        ViewExtension.ext(asset_no_prop).incidence_angle
+        == ViewExtension.ext(item).incidence_angle
+    )
     assert ViewExtension.ext(asset_prop).incidence_angle == 4.0
 
     # Set to Asset
     asset_value = 14.0
     ViewExtension.ext(asset_no_prop).incidence_angle = asset_value
-    assert  ViewExtension.ext(asset_no_prop).incidence_angle != ViewExtension.ext(item).incidence_angle
+    assert (
+        ViewExtension.ext(asset_no_prop).incidence_angle
+        != ViewExtension.ext(item).incidence_angle
+    )
     assert ViewExtension.ext(asset_no_prop).incidence_angle == asset_value
 
     # Validate
     item.validate()
+
 
 @pytest.mark.vcr()
 def test_azimuth(item: Item) -> None:
@@ -122,17 +135,18 @@ def test_azimuth(item: Item) -> None:
     # Get from Asset
     asset_no_prop = item.assets["blue"]
     asset_prop = item.assets["red"]
-    assert  ViewExtension.ext(asset_no_prop).azimuth == ViewExtension.ext(item).azimuth
+    assert ViewExtension.ext(asset_no_prop).azimuth == ViewExtension.ext(item).azimuth
     assert ViewExtension.ext(asset_prop).azimuth == 5.0
 
     # Set to Asset
     asset_value = 15.0
     ViewExtension.ext(asset_no_prop).azimuth = asset_value
-    assert  ViewExtension.ext(asset_no_prop).azimuth != ViewExtension.ext(item).azimuth
+    assert ViewExtension.ext(asset_no_prop).azimuth != ViewExtension.ext(item).azimuth
     assert ViewExtension.ext(asset_no_prop).azimuth == asset_value
 
     # Validate
     item.validate()
+
 
 @pytest.mark.vcr()
 def test_sun_azimuth(item: Item) -> None:
@@ -144,21 +158,28 @@ def test_sun_azimuth(item: Item) -> None:
 
     # Set
     ViewExtension.ext(item).sun_azimuth = view_sun_azimuth + 100
-    assert  view_sun_azimuth + 100 == item.properties["view:sun_azimuth"]
+    assert view_sun_azimuth + 100 == item.properties["view:sun_azimuth"]
     # Get from Asset
     asset_no_prop = item.assets["blue"]
     asset_prop = item.assets["red"]
-    assert  ViewExtension.ext(asset_no_prop).sun_azimuth == ViewExtension.ext(item).sun_azimuth
+    assert (
+        ViewExtension.ext(asset_no_prop).sun_azimuth
+        == ViewExtension.ext(item).sun_azimuth
+    )
     assert ViewExtension.ext(asset_prop).sun_azimuth == 1.0
 
     # Set to Asset
     asset_value = 11.0
     ViewExtension.ext(asset_no_prop).sun_azimuth = asset_value
-    assert  ViewExtension.ext(asset_no_prop).sun_azimuth != ViewExtension.ext(item).sun_azimuth
+    assert (
+        ViewExtension.ext(asset_no_prop).sun_azimuth
+        != ViewExtension.ext(item).sun_azimuth
+    )
     assert ViewExtension.ext(asset_no_prop).sun_azimuth == asset_value
 
     # Validate
     item.validate()
+
 
 @pytest.mark.vcr()
 def test_sun_elevation(item: Item) -> None:
@@ -170,21 +191,28 @@ def test_sun_elevation(item: Item) -> None:
 
     # Set
     ViewExtension.ext(item).sun_elevation = view_sun_elevation + 10
-    assert  view_sun_elevation + 10 == item.properties["view:sun_elevation"]
+    assert view_sun_elevation + 10 == item.properties["view:sun_elevation"]
     # Get from Asset
     asset_no_prop = item.assets["blue"]
     asset_prop = item.assets["red"]
-    assert  ViewExtension.ext(asset_no_prop).sun_elevation == ViewExtension.ext(item).sun_elevation
+    assert (
+        ViewExtension.ext(asset_no_prop).sun_elevation
+        == ViewExtension.ext(item).sun_elevation
+    )
     assert ViewExtension.ext(asset_prop).sun_elevation == 2.0
 
     # Set to Asset
     asset_value = 12.0
     ViewExtension.ext(asset_no_prop).sun_elevation = asset_value
-    assert  ViewExtension.ext(asset_no_prop).sun_elevation != ViewExtension.ext(item).sun_elevation
+    assert (
+        ViewExtension.ext(asset_no_prop).sun_elevation
+        != ViewExtension.ext(item).sun_elevation
+    )
     assert ViewExtension.ext(asset_no_prop).sun_elevation == asset_value
 
     # Validate
     item.validate()
+
 
 def test_extension_not_implemented(item: Item) -> None:
     # Should raise exception if Item does not include extension URI
@@ -203,6 +231,7 @@ def test_extension_not_implemented(item: Item) -> None:
     ownerless_asset = pystac.Asset.from_dict(asset.to_dict())
     _ = ViewExtension.ext(ownerless_asset)
 
+
 def test_item_ext_add_to(item: Item) -> None:
     item.stac_extensions.remove(ViewExtension.get_schema_uri())
     assert ViewExtension.get_schema_uri() not in item.stac_extensions
@@ -210,6 +239,7 @@ def test_item_ext_add_to(item: Item) -> None:
     _ = ViewExtension.ext(item, add_if_missing=True)
 
     assert ViewExtension.get_schema_uri() in item.stac_extensions
+
 
 def test_asset_ext_add_to(item: Item) -> None:
     item.stac_extensions.remove(ViewExtension.get_schema_uri())
@@ -220,10 +250,10 @@ def test_asset_ext_add_to(item: Item) -> None:
 
     assert ViewExtension.get_schema_uri() in item.stac_extensions
 
+
 def test_should_raise_exception_when_passing_invalid_extension_object() -> None:
     with pytest.raises(
-        ExtensionTypeError,
-        match=r"^ViewExtension does not apply to type 'object'$"
+        ExtensionTypeError, match=r"^ViewExtension does not apply to type 'object'$"
     ):
         # calling it wrong on purpose so ------v
         ViewExtension.ext(object())  # type: ignore
@@ -236,14 +266,14 @@ def test_get_off_nadir_summaries(collection: Collection) -> None:
 
     assert {"minimum": 0.5, "maximum": 7.3} == off_nadirs.to_dict()
 
+
 def test_get_incidence_angle_summaries(collection: Collection) -> None:
-    incidence_angles = ViewExtension.summaries(
-        collection, True
-    ).incidence_angle
+    incidence_angles = ViewExtension.summaries(collection, True).incidence_angle
 
     assert incidence_angles is not None
 
     assert {"minimum": 23, "maximum": 35} == incidence_angles.to_dict()
+
 
 def test_get_azimuth_summaries(collection: Collection) -> None:
     azimuths = ViewExtension.summaries(collection, True).azimuth
@@ -252,12 +282,14 @@ def test_get_azimuth_summaries(collection: Collection) -> None:
 
     assert {"minimum": 20, "maximum": 186} == azimuths.to_dict()
 
+
 def test_get_sun_azimuth_summaries(collection: Collection) -> None:
     sun_azimuths = ViewExtension.summaries(collection, True).sun_azimuth
 
     assert sun_azimuths is not None
 
     assert {"minimum": 48, "maximum": 78} == sun_azimuths.to_dict()
+
 
 def test_get_sun_elevation_summaries(collection: Collection) -> None:
     sun_elevations = ViewExtension.summaries(collection, True).sun_elevation
@@ -266,17 +298,20 @@ def test_get_sun_elevation_summaries(collection: Collection) -> None:
 
     assert {"minimum": 10, "maximum": 45} == sun_elevations.to_dict()
 
+
 def test_set_off_nadir_summaries(collection: Collection) -> None:
     view_summaries = ViewExtension.summaries(collection, True)
 
     view_summaries.off_nadir = RangeSummary(0, 10)
-    assert  {"minimum": 0, "maximum": 10} == view_summaries.off_nadir.to_dict()
+    assert {"minimum": 0, "maximum": 10} == view_summaries.off_nadir.to_dict()
+
 
 def test_set_incidence_angle_summaries(collection: Collection) -> None:
     view_summaries = ViewExtension.summaries(collection, True)
 
     view_summaries.incidence_angle = RangeSummary(5, 15)
-    assert  {"minimum": 5, "maximum": 15} == view_summaries.incidence_angle.to_dict()
+    assert {"minimum": 5, "maximum": 15} == view_summaries.incidence_angle.to_dict()
+
 
 def test_set_azimuth_summaries(collection: Collection) -> None:
     view_summaries = ViewExtension.summaries(collection, True)
@@ -284,17 +319,20 @@ def test_set_azimuth_summaries(collection: Collection) -> None:
     view_summaries.azimuth = None
     assert view_summaries.azimuth is None
 
+
 def test_set_sun_azimuth_summaries(collection: Collection) -> None:
     view_summaries = ViewExtension.summaries(collection, True)
 
     view_summaries.sun_azimuth = RangeSummary(210, 275)
-    assert  {"minimum": 210, "maximum": 275} == view_summaries.sun_azimuth.to_dict()
+    assert {"minimum": 210, "maximum": 275} == view_summaries.sun_azimuth.to_dict()
+
 
 def test_set_sun_elevation_summaries(collection: Collection) -> None:
     view_summaries = ViewExtension.summaries(collection, True)
 
     view_summaries.sun_elevation = RangeSummary(-10, 38)
-    assert  {"minimum": -10, "maximum": 38} == view_summaries.sun_elevation.to_dict()
+    assert {"minimum": -10, "maximum": 38} == view_summaries.sun_elevation.to_dict()
+
 
 def test_summaries_adds_uri(collection: Collection) -> None:
     collection.stac_extensions = []
