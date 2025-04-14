16bfdcd527813a4ead3be1b117ab31d2b2fe5fbd
= <jonhur@web.de>
2025-04-14 10:34:04 +0200

    line break to satisfy line length requirement

--- pystac/extensions/mlm.py
+++ pystac/extensions/mlm.py
@@ -490,8 +490,8 @@ class ValueScaling:
         cls, type: ValueScalingType, props: dict[str, Any]
     ) -> None:
         """
-        Validate whether given properties satisfy the requirements set by the ValueScaling
-            ``type`` parameter
+        Validate whether given properties satisfy the requirements set by the
+            ValueScaling ``type`` parameter
 
         Args:
             type: The type of ValueScaling operation
