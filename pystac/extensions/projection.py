897f3cbec7ebbf599ba44c6c0931d88918ab68a4
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2025-04-15 11:03:02 -0400

    Projection extension: migrate Assets on Items (#1549)
    * Migrate projection extension on assets
    
    * Add migrate test for collection item-assets
    
    * Add 1549 to changelog
    
    * Fix asset+item_asset migration handling

--- pystac/extensions/projection.py
+++ pystac/extensions/projection.py
@@ -480,6 +480,11 @@ class ProjectionExtensionHooks(ExtensionHooks):
         if epsg := obj["properties"].pop("proj:epsg", None):
             obj["properties"]["proj:code"] = f"EPSG:{epsg}"
 
+        for key in ["assets", "item_assets"]:
+            for asset in obj.get(key, {}).values():
+                if epsg := asset.pop("proj:epsg", None):
+                    asset["proj:code"] = f"EPSG:{epsg}"
+
         super().migrate(obj, version, info)
 
 
