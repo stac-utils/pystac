eab66931a14cadff266063cd889681c85feacd01
Julia Signell <jsignell@gmail.com>
2026-04-23 11:30:00 -0400

    Fix item_collection

--- src/pystac/item_collection.py
+++ src/pystac/item_collection.py
@@ -5,6 +5,10 @@ from collections.abc import Iterator
 from pathlib import Path
 from typing import Any, Literal, TypedDict, override
 
+from typing_extensions import deprecated
+
+from .container import Container
+from .errors import STACTypeError
 from .item import Item
 from .reader import DEFAULT_READER, Reader
 from .utils import make_absolute_href
@@ -17,8 +21,17 @@ class T_ItemCollection(TypedDict):
 
 class ItemCollection:
     def __init__(self, items: list[Item], **kwargs: Any):
-        self.items: list[Item] = items
-        self.extra_fields: dict[str, Any] = kwargs
+        self.items: list[Item] = [Item.try_from(item) for item in items]
+
+        if "root" in kwargs:
+            raise
+
+        self.extra_fields: dict[str, Any]
+        if "extra_fields" in kwargs:
+            self.extra_fields = kwargs.pop("extra_fields")
+            assert len(kwargs) == 0
+        else:
+            self.extra_fields = kwargs
 
     def __len__(self) -> int:
         return len(self.items)
@@ -28,6 +41,16 @@ class ItemCollection:
 
     def __iter__(self) -> Iterator[Item]:
         return iter(self.items)
+
+    def __contains__(self, __x: Item) -> bool:
+        return __x in self.items
+
+    def __add__(self, other: Any) -> ItemCollection:
+        if not isinstance(other, ItemCollection):
+            return NotImplemented
+
+        combined = [*self.items, *other.items]
+        return ItemCollection(items=combined)
 
     @override
     def __repr__(self) -> str:
@@ -41,6 +64,14 @@ class ItemCollection:
             **data,
         }
 
+    def clone(self) -> ItemCollection:
+        return copy.deepcopy(self)
+
+    @deprecated("Use `ItemCollection.from_dict` instead")
+    @staticmethod
+    def is_item_collection(data: dict[str, Any]) -> bool:
+        return data.get("type", "") == "FeatureCollection"
+
     @classmethod
     def try_from(cls, data: dict[str, Any] | ItemCollection) -> ItemCollection:
         if isinstance(data, ItemCollection):
@@ -53,14 +84,20 @@ class ItemCollection:
         cls,
         data: dict[str, Any],
         preserve_dict: bool = True,
+        root: Container | None = None,
     ) -> ItemCollection:
+        if data.get("type", "") != "FeatureCollection":
+            raise STACTypeError(data, cls)
+
         if preserve_dict:
             data = copy.deepcopy(data)
 
         items = data.get("features", [])
         extra_fields = {k: v for k, v in data.items() if k not in ("features", "type")}
 
-        return cls(items=[Item.from_dict(item) for item in items], **extra_fields)
+        return cls(
+            items=[Item.from_dict(item, root=root) for item in items], **extra_fields
+        )
 
     @classmethod
     def from_file(
